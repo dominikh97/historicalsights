372bce248a00f8d20964ab9fe528adcd8d61a284
dominikh97 <115783685+dominikh97@users.noreply.github.com>
2024-12-21 20:07:22 +0100

    Remove marker

--- map.js
+++ map.js
@@ -5,8 +5,3 @@ const map = L.map('map').setView([51.505, -0.09], 13); // Initial view at lat: 5
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-// Optionally, add a marker on the map to confirm it's working
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup("A historical location")
-    .openPopup();
