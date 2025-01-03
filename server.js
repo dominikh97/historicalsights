8fe86c50d5962671d6fd65c12feb74811485db09
dominikh97 <115783685+dominikh97@users.noreply.github.com>
2025-01-03 12:21:39 +0100

    Update server.js (delete second port declaration)

--- server.js
+++ server.js
@@ -140,9 +140,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     logInfo(`Server running on http://localhost:${port}`);
 });
-
-// Set Port configuration
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
